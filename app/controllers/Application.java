aad03c984b15954c777ad2f26e24390f63175111
unknown <msarembock@ZDT-CE683.recm.co.za>
2016-06-07 17:06:36 +0200

    no message

---
+++ app/controllers/Application.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+@With(Secure.class)
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+    
+    public static void mystable() {
+        render();
+    }
+
+}
---
+++ app/controllers/User.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class User 
+{
+	public static boolean authenticate(String user_name, String password)
+
+	{
+		boolean isAuthenticated = true;
+		
+		
+		
+		return isAuthenticated;
+	}
+	
+}
